67f2ef39f2ba5ab3b513a780a0ad9d850a520672
radiantly <buildingsomethingfun@gmail.com>
2021-08-01 23:07:27 +0530

    Buffer input and multithread processing
    Other changes:
    - Fix bug where multiple spaces would increase word count
    - Add basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
+use std::ops::AddAssign;
+#[derive(Debug, PartialEq)]
 pub struct Count {
-    pub chars: u64,
-    pub lines: u64,
-    pub words: u64,
+    pub chars: usize,
+    pub lines: usize,
+    pub words: usize,
 }
 
 impl Count {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Count {
             chars: 0,
             lines: 0,
@@ -14,14 +16,26 @@ impl Count {
     }
 }
 
-pub fn parse(buf: &Vec<u8>) -> Count {
+impl AddAssign for Count {
+    fn add_assign(&mut self, rhs: Self) {
+        self.chars += rhs.chars;
+        self.lines += rhs.lines;
+        self.words += rhs.words;
+    }
+}
+
+pub fn parse(buf: Vec<u8>) -> Count {
     let mut count = Count::new();
+    count.chars = buf.len();
     let mut last_char_was_space = true;
     for c in buf {
-        if !last_char_was_space && *c == ' ' as u8 {
+        if c == ' ' as u8 {
+            if last_char_was_space {
+                continue;
+            }
             count.words += 1;
             last_char_was_space = true;
-        } else if *c == '\n' as u8 {
+        } else if c == '\n' as u8 {
             if !last_char_was_space {
                 count.words += 1;
                 last_char_was_space = true;
@@ -30,11 +44,57 @@ pub fn parse(buf: &Vec<u8>) -> Count {
         } else {
             last_char_was_space = false;
         }
-        count.chars += 1
     }
 
     if !last_char_was_space {
         count.words += 1;
     }
     count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_count(chars: usize, lines: usize, words: usize) -> Count {
+        Count {
+            chars,
+            lines,
+            words,
+        }
+    }
+
+    #[test]
+    fn no_words() {
+        assert_eq!(parse(vec![]), create_count(0, 0, 0));
+    }
+
+    #[test]
+    fn one_word() {
+        assert_eq!(parse("hello".as_bytes().into()), create_count(5, 1, 1));
+    }
+
+    #[test]
+    fn basic_phrase() {
+        assert_eq!(
+            parse(
+                "Hi you seem to have an air oh mystery surrounding you"
+                    .as_bytes()
+                    .into()
+            ),
+            create_count(53, 1, 11)
+        );
+    }
+
+    #[test]
+    fn bad_space() {
+        assert_eq!(
+            parse(
+                "  There  seems  to be a weird number   of spaces    here. Super unusual, right?   "
+                    .as_bytes()
+                    .into()
+            ),
+            create_count(82, 1, 13)
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,60 @@
-use std::io::{self, Error, Read};
+use std::{
+    error::Error,
+    io::{self, Read},
+    sync::{Arc, Condvar, Mutex},
+    thread,
+};
 
-fn main() -> Result<(), Error> {
-    let mut buf = Vec::new();
-    io::stdin().read_to_end(&mut buf)?;
-    let count = wcr::parse(&buf);
-    println!(
-        "Words: {}\nLines: {}\nBytes: {}",
-        count.words, count.lines, count.chars
-    );
+fn main() -> Result<(), Box<dyn Error>> {
+    let chunk_size = 0x1000000usize;
+    let final_count = Arc::new(Mutex::new(wcr::Count::new()));
+
+    let max_threads = num_cpus::get();
+
+    let pair = Arc::new((Mutex::new(0usize), Condvar::new()));
+    let (lock, cvar) = &*pair;
+
+    loop {
+        let mut chunk = Vec::with_capacity(chunk_size);
+        let n = io::stdin()
+            .take(chunk_size as u64)
+            .read_to_end(&mut chunk)?;
+        if n == 0 {
+            break;
+        }
+
+        let fcount = final_count.clone();
+
+        let mut thread_count = lock.lock().unwrap();
+        while *thread_count >= max_threads {
+            thread_count = cvar.wait(thread_count).unwrap();
+        }
+
+        *thread_count += 1;
+
+        let thread_pair = pair.clone();
+        thread::spawn(move || {
+            let count = wcr::parse(chunk);
+            let mut fc = fcount.lock().unwrap();
+            *fc += count;
+            let (lock, cvar) = &*thread_pair;
+
+            let mut thread_count = lock.lock().unwrap();
+            *thread_count -= 1;
+            cvar.notify_one();
+        });
+    }
+
+    let _guard = cvar
+        .wait_while(lock.lock().unwrap(), |thread_count| thread_count > &mut 0)
+        .unwrap();
+
+    let mut count = final_count.lock().unwrap();
+
+    if count.chars > 0 {
+        count.lines += 1;
+    }
+
+    println!("{:?}", count);
     Ok(())
 }
